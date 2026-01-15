d018484c73109b5da3f0a40c953b98e5735997f8
mimouni nihad <nihadmimouni224@gmail.com>
2026-01-15 20:48:38 +0100

    With fact Iterative function

--- combinations.py
+++ combinations.py
@@ -1,16 +1,10 @@
 k = int (input("Introduire k svp : "))
 n = int (input("Introduire n svp : "))
-f1=1
-for i in range(2,n+1):
-    f1 = f1 * i
-print(f1)
-f2=1
-for i in range(2,k+1):
-    f2 = f2 * i
-print(f2)
-f3=1
-for i in range(2,n-k+1):
-    f3 = f3 * i
-print(f3)
+def fact(n):
+    res = 1
+    for i in range(1, n + 1):
+        res = res * i
+    return res
+result = fact(n) / (fact(k) * fact(n - k))
 
-print("comb de ", k, ",", n," = ", f1//(f2 * f3))
+print("Result using Iterative function is:", result)
