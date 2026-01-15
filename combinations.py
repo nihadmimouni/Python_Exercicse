6cd51cc41c4dd475ef18f67c8d280b4a900c66a5
mimouni nihad <nihadmimouni224@gmail.com>
2026-01-15 21:00:07 +0100

    Using the math library

--- combinations.py
+++ combinations.py
@@ -1,11 +1,8 @@
 k = int (input("Introduire k svp : "))
 n = int (input("Introduire n svp : "))
-def fact(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * fact(n - 1)
-        
-result = fact(n) / (fact(k) * fact(n - k))
 
-print("Result using Recursive function is:", result)
+import math
+
+result = math.comb(n, k)
+
+print("Result using math library is:", result)
