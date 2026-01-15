6861e59c6748b033a3541e2ca87ac76774884ac7
mimouni nihad <nihadmimouni224@gmail.com>
2026-01-15 20:54:59 +0100

    With fact Recursive function

--- combinations.py
+++ combinations.py
@@ -1,10 +1,11 @@
 k = int (input("Introduire k svp : "))
 n = int (input("Introduire n svp : "))
 def fact(n):
-    res = 1
-    for i in range(1, n + 1):
-        res = res * i
-    return res
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+        
 result = fact(n) / (fact(k) * fact(n - k))
 
-print("Result using Iterative function is:", result)
+print("Result using Recursive function is:", result)
